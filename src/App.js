f8f13cb33d95e6e946b4a29f94e6a67ba3950289
Maryia Biandeha <yauheniznak@gmail.com>
2023-01-30 21:33:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+   const [tips,setTips] = useState ("");
+
+ const abc = async()=>{
+  const response = await fetch (`http://www.boredapi.com/api/activity/`);
+  const data = await response.json();
+  setTips(data.activity);
+ }
+
+ useEffect(()=>{
+  abc()
+ }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+<p>{tips}</p>
+<button onClick={abc} >Find your activity today...</button>
     </div>
-  );
+  )
 }
+  
+
 
 export default App;
+ 
